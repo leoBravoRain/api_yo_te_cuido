d6ba1987ac35198b17f715918e8b1ff9f2ea6b65
Gian <gian.bravo@alumnos.usm.cl>
2019-02-20 13:45:31 -0300

    add id_management_system to Danger model

--- yo_te_ciudo_API/api/models.py
+++ yo_te_ciudo_API/api/models.py
@@ -25,6 +25,10 @@ class Dangers(models.Model):
 
     floor_number = models.CharField(max_length = 2000)
 
+    # Id for local managment system of risk of each company (each company has its own MS)
+    # It can be an empty value
+    id_management_system = models.CharField(max_length = 2000, blank = True)
+
     # # Metodo para obtener nombre de objeto
     # def __unicode__(self):
     # 	return self.id
